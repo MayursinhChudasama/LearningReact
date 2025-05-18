26428089d5fb9739c4705d87820032777fb5b349
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-05-18 17:50:32 +0530

    Sec-8:145. Enhancing the Demo App "Result Modal" Sec-8:146. Closing the Modal via the ESC (Escape) Key
    fix: ResultModal component for improved readability and score calculation

--- sec-08-refs and portal/src/components/ResultModal.jsx
+++ sec-08-refs and portal/src/components/ResultModal.jsx
@@ -1,11 +1,18 @@
-export default function ResultModal({ ref, targetTime, remainingTime, onReset }) {
+export default function ResultModal({
+  ref,
+  targetTime,
+  remainingTime,
+  onReset,
+}) {
   const userLost = remainingTime <= 0;
   const formattedRemainingTime = (remainingTime / 1000).toFixed(2);
+  const score = Math.round((1 - remainingTime / (targetTime * 1000)) * 100);
   return (
     <dialog
       ref={ref}
       className='result-modal'>
-      {userLost &&<h2>You Lost</h2>}
+      {userLost && <h2>You Lost</h2>}
+      {!userLost && <h2>Your Score: {score}</h2>}
       <p>
         The target time was <strong>{targetTime}</strong> second
         {targetTime > 1 ? "s" : ""}
@@ -15,7 +22,7 @@ export default function ResultModal({ ref, targetTime, remainingTime, onReset })
         <strong>{formattedRemainingTime} second left</strong>
       </p>
       <form action='dialog'>
-        <button onSubmit={onReset}>Close</button>
+        <button onClose={onReset}>Close</button>
       </form>
     </dialog>
   );
