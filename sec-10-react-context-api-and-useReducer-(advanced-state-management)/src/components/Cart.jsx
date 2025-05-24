bf3124496d2b2aa83494d15ea62c032e671b510d
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-05-24 10:33:40 +0530

    sec-10:173. Migrating the Entire Demo Project to use the Context API
    feat: refactor cart context and components to streamline item quantity updates

--- sec-10-react-context-api-and-useReducer-(advanced-state-management)/src/components/Cart.jsx
+++ sec-10-react-context-api-and-useReducer-(advanced-state-management)/src/components/Cart.jsx
@@ -1,8 +1,8 @@
 import { useContext } from "react";
 import { CartContext } from "../store/cart-context.jsx";
-export default function Cart({ onUpdateItemQuantity }) {
-  const { items } = useContext(CartContext);
-  // const cartCtx = useContext(CartContext);
+export default function Cart() {
+  const { items, updateCartItemQuantity } = useContext(CartContext);
+
   const totalPrice = items.reduce(
     (acc, item) => acc + item.price * item.quantity,
     0
@@ -24,11 +24,11 @@ export default function Cart({ onUpdateItemQuantity }) {
                   <span> ({formattedPrice})</span>
                 </div>
                 <div className='cart-item-actions'>
-                  <button onClick={() => onUpdateItemQuantity(item.id, -1)}>
+                  <button onClick={() => updateCartItemQuantity(item.id, -1)}>
                     -
                   </button>
                   <span>{item.quantity}</span>
-                  <button onClick={() => onUpdateItemQuantity(item.id, 1)}>
+                  <button onClick={() => updateCartItemQuantity(item.id, 1)}>
                     +
                   </button>
                 </div>
