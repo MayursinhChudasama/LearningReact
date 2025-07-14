d9289fe02a5b8598852bc439a02219aab0c1f35c
MayursinhChudasama <contact.mayursinh@gmail.com>
2025-07-14 12:11:43 +0530

    sec-32:react+typescript

--- sec-28-animating-apps/src/components/Modal.jsx
+++ sec-28-animating-apps/src/components/Modal.jsx
@@ -1,13 +1,21 @@
 import { createPortal } from 'react-dom';
-
+import {motion} from 'framer-motion';
 export default function Modal({ title, children, onClose }) {
   return createPortal(
     <>
       <div className="backdrop" onClick={onClose} />
-      <dialog open className="modal">
+      <motion.dialog   onClick={(e) => e.stopPropagation()}  
+      variants={{
+        hidden: { opacity: 0 , y: 30},
+        visible: { opacity: 1 , y: 0}
+      }}
+      animate="visible"
+      initial="hidden"
+      exit="hidden"
+      open className="modal">
         <h2>{title}</h2>
         {children}
-      </dialog>
+      </motion.dialog>
     </>,
     document.getElementById('modal')
   );
